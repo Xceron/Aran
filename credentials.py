ffc80de8fd4938ae73547e84b7e972e3ca714bc6
Xceron <brand.f@icloud.com>
2019-03-03 21:42:14 +0100

    added file for encryption

---
+++ credentials.py
@@ -0,0 +1,18 @@
+import keyring
+
+
+def save_credentials(username, password):
+    """
+    :param username: username used for Studip
+    :param password: password used for Studip
+    :return: sets username and password in local keyring
+    """
+    keyring.set_password("StudipCrawl", username, password)
+
+
+def get_credentials(username):
+    """
+    :param username: username used for Studip
+    :return: password
+    """
+    return keyring.get_password("StudipCrawl",username)
