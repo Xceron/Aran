b1bfdf7f2be734d83f734e51574cfef0c33eddc8
xceron <github@florianbrand.de>
2019-11-18 21:56:22 +0100

    changed nested dict to list, added possibility to add synonyms

--- aran/filehandling.py
+++ aran/filehandling.py
@@ -2,66 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import os
-from collections import Iterable
-from functools import reduce
-from collections.abc import MutableMapping
+import asyncio
+import json
 
 
 def make_folder_name(old_name: str) -> str:
     """
-    :param old_name: unformatted name
+    :param old_name: not formatted name
     :return: gets rid of useless words for a easier to find folder name in dst_folder
     """
+    json_file = open(os.path.join(os.getcwd(), "aran_config.json"))
+    json_data = json.load(json_file)["synonyms"]
+    for key, values in json_data.items():
+        if key in old_name:
+            return values
     old_name = old_name.strip()
     for sign in ["Vorlesung: ", "Übung: ", "Tutorium: ", " - Dateien", "sonstige: "]:
         old_name = old_name.replace(sign, "")
     return old_name
 
 
-def get_directory_structure(rootdir: str) -> dict:
+async def get_file_size_of_dir(root_dir: str) -> list:
     """
-    Creates a nested dictionary that represents the folder structure of rootdir
+    Creates a list that represents the folder structure of root_dir
     """
-    return_dict = {}
-    rootdir = rootdir.rstrip(os.sep)
-    start = rootdir.rfind(os.sep) + 1
-    for root, dirs, files in os.walk(rootdir):
-        folders = root[start:].split(os.sep)
-        subdir = dict.fromkeys(files)
-        for name in files:
-            subdir[name] = os.stat(os.path.join(root, name)).st_size
-        parent = reduce(dict.get, folders[:-1], return_dict)
-        parent[folders[-1]] = subdir
-    return return_dict
-
-
-def find_key(dictionary: dict, key: object) -> object:
-    try:
-        if key in dictionary:
-            return dictionary[key]
-        for key_di, sub_dictionary in dictionary.items():
-            val = find_key(sub_dictionary, key)
-            if val:
-                return val
-    except TypeError:
-        return None
-
-
-def flatten(d: dict, parent_key: object = '', sep: object = '_') -> dict:
-    items = []
-    for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, MutableMapping):
-            items.extend(flatten(v, new_key, sep=sep).items())
-        else:
-            items.append((new_key, v))
-    return dict(items)
-
-
-def find_parent_keys(d: dict, target_key: object, parent_key: object = None) -> Iterable:
-    for k, v in d.items():
-        if k == target_key:
-            yield parent_key
-        if isinstance(v, dict):
-            for res in find_parent_keys(v, target_key, k):
-                yield res
+    return_list = []
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            try:
+                return_list.append(os.stat(os.path.join(root, file)).st_size)
+            except FileNotFoundError:
+                pass
+    return return_list
